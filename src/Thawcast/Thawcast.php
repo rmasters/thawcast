6a0bf8e66a34658cc06b2b9944cdede583266354
Ross Masters <ross@rossmasters.com>
2012-11-24 23:20:18 +0000

    Base class

---
+++ src/Thawcast/Thawcast.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Thawcast
+ * @author Ross Masters <ross@rossmasters.com>
+ */
+
+namespace Thawcast;
+
+use InvalidArgumentException, LengthException, DomainException;
+
+/**
+ * Thawcast 
+ */
+class Thawcast
+{
+    private $host;
+    private $credentials;
+
+    /**
+     * Constructor
+     * @param string $host URL of the Icecast2 server (assumed port 80 if missing)
+     * @param array|null $credentials Admin login in the form array(user, pass)
+     * @throws InvalidArgumentException, LengthException
+     */
+    public function __construct($host, $credentials=null) {
+        $this->setHost($host);
+
+        // Set admin credentials
+        if (!is_null($credentials)) {
+            if (!is_array($credentials)) {
+                throw new InvalidArgumentException('Credentials should be ' .
+                    'supplied as an array of two strings');
+            }
+
+            if (count($credentials) != 2) {
+                throw new LengthException('Credentials array should contain ' .
+                    'a username and a password');
+            }
+
+            $credentials = array_values($credentials);
+            $this->setCredentials($credentials[0], $credentials[1]);
+        }
+    }
+
+    /**
+     * Get the url of the Icecast web interface
+     * @return string
+     */
+    public function getHost() {
+        return $this->host;
+    }
+
+    /**
+     * Validate and set the Icecast url
+     * @param string $url
+     * @throws DomainException, InvalidArgumentException
+     * @return $this
+     */
+    public function setHost($url) {
+        $urlParts = parse_url($url);
+        if (false === $urlParts) {
+            throw new DomainException('Icecast URL is malformed.');
+        }
+
+        if (!array_key_exists('host', $urlParts)) {
+            throw new InvalidArgumentException('Icecast URL has no host');
+        }
+
+        // Assume http if missing
+        if (!array_key_exists('scheme', $urlParts)) {
+            $urlParts['scheme'] = 'http';
+        }
+
+        // Default Icecast2 web interface port is 8000
+        if (!array_key_exists('port', $urlParts)) {
+            $urlParts['port'] = 8000;
+        }
+
+        if (!array_key_exists('path', $urlParts)) {
+            $urlParts['path'] = '/';
+        }
+
+        // Reconstruct the URL, using the scheme, host, port and a path
+        // HTTP credentials are omitted, as they should be given in setCredentials()
+        $url = sprintf('%s://%s:%d/%s', $urlParts['scheme'], $urlParts['host'],
+            $urlParts['port'], $urlParts['path']);
+
+        // Ensure a trailing slash is present
+        $this->host = rtrim($url, '/') . '/';
+
+        return $this;
+    }
+
+    /**
+     * Get the admin credentials, or throw an Exception if not set
+     * @return array
+     * @throws Exception If credentials are not set
+     */
+    public function getCredentials() {
+        if (!$this->hasCredentials()) {
+            throw new Exception('This instance has no admin credentials.');
+        }
+        return $this->credentials;
+    }
+
+    /**
+     * Store admin credentials to use for admin functions (optional)
+     * @param string $username
+     * @param string $password
+     * @return $this
+     */
+    public function setCredentials($username, $password) {
+        $this->credentials = array(
+            'username' => (string) $username,
+            'password' => (string) $password
+        );
+
+        return $this;
+    }
+
+    /**
+     * Whether the instance has been provided with admin credentials
+     * @return bool
+     */
+    public function hasCredentials() {
+        return is_array($this->credentials);
+    }
+
+    /**
+     * Get the Icecast host
+     * @return string
+     */
+    public function __toString() {
+        return (string) $this->host;
+    }
+}
---
+++ tests/Thawcast/ThawcastTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Thawcast;
+
+use PHPUnit_Framework_TestCase;
+
+class ThawcastTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstructor() {
+        // Non-admin privileged
+        $thawcast = new Thawcast('http://localhost');
+        $this->assertEquals('http://localhost:8000/', $thawcast->getHost());
+        $this->assertFalse($thawcast->hasCredentials());
+
+        // Admin privileged
+        $thawcast = new Thawcast('http://localhost', array('admin', 'password'));
+        $this->assertTrue($thawcast->hasCredentials());
+        $this->assertEquals(array('username' => 'admin', 'password' => 'password'), $thawcast->getCredentials());
+
+        // Admin privileged (wrong number of credential elements)
+        $this->setExpectedException('LengthException');
+        $thawcast = new Thawcast('http://localhost', array('admin'));
+
+        // Admin privileged (invalid credentials argument)
+        $this->setExpectedException('InvalidArgumentException');
+        $thawcast = new Thawcast('http://localhost', 'admin');
+    }
+
+    public function testHost() {
+        // Without port
+        $thawcast = new Thawcast('http://localhost');
+        $this->assertEquals('http://localhost:8000/', $thawcast->getHost());
+
+        // With port
+        $thawcast->setHost('http://localhost:1234');
+        $this->assertEquals('http://localhost:1234/', $thawcast->getHost());
+
+        // Without scheme or host
+        $this->setExpectedException('InvalidArgumentException');
+        $thawcast->setHost('localhost');
+
+        // Without scheme
+        $thawcast->setHost('//localhost');
+        $this->assertEquals('http://localhost:8000/', $thawcast->getHost());
+
+        // With HTTP credentials
+        $thawcast->setHost('http://user:pass@localhost');
+        $this->assertEquals('http://localhost:8000/', $thawcast->getHost());
+
+        // Malformed
+        $this->setExpectedException('DomainException');
+        $thawcast->setHost('@#!');
+    }
+
+    public function testAdminCredentials() {
+        $thawcast = new Thawcast('http://localhost');
+
+        $thawcast->setCredentials('admin', 'password');
+        $this->assertEquals(array('username' => 'admin', 'password' => 'password'), $thawcast->getCredentials());
+    }
+
+    public function testToString() {
+        $thawcast = new Thawcast('http://localhost');
+        $this->assertEquals('http://localhost:8000/', (string) $thawcast);
+    }
+}
